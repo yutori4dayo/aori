437bf6f5b0e6d4fc24c154aa2cac2d57d3c54b2d
yutori4dayo <yutori4dayo@gmail.com>
2020-04-22 13:09:50 +0900

    ファビコン変更

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -28,7 +28,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset(' css/mains.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/mains.css') }}">
     <script src="https://kit.fontawesome.com/c2064407e9.js"></script>
     <title>あおり運転ナンバー投稿</title>
   </head>
