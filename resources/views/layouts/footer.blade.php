0a86663f0999f4cb5a7300ba02ecb75a191b38ba
yutori4dayo <yutori4dayo@gmail.com>
2020-05-04 00:58:56 +0900

    コピーライト追加

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,9 @@
+@php
+  $thisyear =date('Y');
+@endphp
+<div class="copyright text-center pb-3">
+  <small>&copy; 2020-{{$thisyear}}  All Rights Reserved.<a href="https://twitter.com/aoriuntennet">@aoriuntennet</a></small>
+</div>
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
