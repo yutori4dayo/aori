a5057172105c1de840011a5a72c9dc9c22d6f8c6
安東嘉章 <yutori4dayo@gmail.com>
2020-05-30 00:40:56 +0900

    migration 修正

--- database/migrations/2020_05_10_180256_create_affiliate_table.php
+++ database/migrations/2020_05_10_180256_create_affiliate_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAffiliates extends Migration
+class CreateAffiliateTable extends Migration
 {
     /**
      * Run the migrations.
